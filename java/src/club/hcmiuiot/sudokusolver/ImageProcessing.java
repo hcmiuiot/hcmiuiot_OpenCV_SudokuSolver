ccfef3d55bb1e8db209bb78bc1bcca295096a8fb
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2017-12-20 22:38:37 +0700

    KNN

--- java/src/club/hcmiuiot/sudokusolver/ImageProcessing.java
+++ java/src/club/hcmiuiot/sudokusolver/ImageProcessing.java
@@ -10,6 +10,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -18,18 +19,25 @@ import org.opencv.utils.Converters;
 import org.opencv.videoio.VideoCapture;
 
 import club.hcmiuiot.opencv.ImgShow;
+import club.hcmiuiot.vnOCR.OCRKNN;
 
 public class ImageProcessing {
 	
 	public void process() {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		OCRKNN.train();
+		
 		VideoCapture vc = new VideoCapture(0);
 		Mat frame = new Mat();
 		Mat blurred = new Mat();
 		Mat gray = new Mat();
 		Mat thresholded = new Mat();
+		Mat grayImg = new Mat();
+		
 		while (true) {
 			vc.read(frame);
+			Imgproc.cvtColor(frame, grayImg, Imgproc.COLOR_BGR2GRAY);
 			//ImgShow.imshow("src", frame);
 			Imgproc.GaussianBlur(frame, blurred, new Size(11,11), 0);
 			//ImgShow.imshow("blurred", blurred);
@@ -86,39 +94,93 @@ public class ImageProcessing {
 				List<Point> srcPoints = new ArrayList<>();
 				Converters.Mat_to_vector_Point2f(approx, srcPoints);
 				
-				for (Point p : srcPoints) {
-					Imgproc.putText(frame, p.toString(), p, 0, 1f, new Scalar(255,0,0));
-					System.out.println(srcPoints);
-				}
+//				for (Point p : srcPoints) {
+//					Imgproc.putText(frame, p.toString(), p, 0, 1f, new Scalar(255,0,0));
+//					//System.out.println(srcPoints);
+//				}
 				
 				List<Point> dstPoints = new ArrayList<>();
+				
+				int size = 600;
 
 				if (srcPoints.get(2).x <= srcPoints.get(0).x &&
 						srcPoints.get(2).y >= srcPoints.get(0).y) {
-					dstPoints.add(new Point(500,0));
+					dstPoints.add(new Point(size,0));
 					dstPoints.add(new Point(0,0));
-					dstPoints.add(new Point(0,500));
-					dstPoints.add(new Point(500,500));
+					dstPoints.add(new Point(0,size));
+					dstPoints.add(new Point(size,size));
 				}
 				else {
 					dstPoints.add(new Point(0,0));
-					dstPoints.add(new Point(0,500));
-					dstPoints.add(new Point(500,500));
-					dstPoints.add(new Point(500,0));
+					dstPoints.add(new Point(0,size));
+					dstPoints.add(new Point(size,size));
+					dstPoints.add(new Point(size,0));
 				}
 				
 				Mat perspectiveForm = Imgproc.getPerspectiveTransform(Converters.vector_Point2f_to_Mat(srcPoints), Converters.vector_Point2f_to_Mat(dstPoints));
 				
 				Mat res = new Mat();
-				Imgproc.warpPerspective(frame, res, perspectiveForm, new Size(500,500));
-				ImgShow.imshow("res", res);
+				Mat resColor = new Mat();
+				
+				Imgproc.warpPerspective(grayImg, res, perspectiveForm, new Size(size,size));
+				Imgproc.warpPerspective(frame, resColor, perspectiveForm, new Size(size,size));
+				//ImgShow.imshow("res", res);
 			
-				ImgShow.imshow("00", subDigit(res, 2, 4));
+				//ImgShow.imshow("00", subDigit(res, 4, 6));
+				Mat textMask = new Mat(size, size, CvType.CV_8U);
+				
+				for (int row=0; row<9; row++) {
+					for (int col=0; col<9; col++) {
+						int predictNum = predictCell(subDigit(res, row, col));
+						double h = res.size().height /9f;
+						double w = res.size().width  /9f;
+						Imgproc.putText(resColor, ""+predictNum, new Point(col*w, row*h+h), Core.FONT_HERSHEY_COMPLEX, 1.2f, new Scalar(255,0,0));
+					}
+				}
+				
+				ImgShow.imshow("data", resColor);
+				
+				System.out.println("Predict at 0,0: " + predictCell(subDigit(res, 4, 6)));
+				//ImgShow.imshow("text", textMask);
 			}
 
 			ImgShow.imshow("contours", frame);
 				
 		}
+	}
+	
+	private static int predictCell(Mat cellImg) {
+		Mat thresholded = new Mat();
+		Imgproc.adaptiveThreshold(cellImg, thresholded, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 15, 2);
+		Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2));
+//		Imgproc.dilate(thresholded, thresholded, kernel);
+		Imgproc.erode(thresholded, thresholded, kernel);
+		Imgproc.dilate(thresholded, thresholded, kernel);
+		
+		List<MatOfPoint> contours =  new ArrayList<>();
+		Mat hierarchy = new Mat();
+		
+		Imgproc.findContours(thresholded, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+		
+		for (int i=0; i<contours.size(); i++) {
+			//Imgproc.drawContours(textMask, contours, i, new Scalar(255,0,0));
+			Rect boundingRec = Imgproc.boundingRect(contours.get(0));
+			if (boundingRec.height >= 0.3f*cellImg.size().height &&
+					boundingRec.width >= 0.05f*cellImg.size().width) {
+				Mat submat = thresholded.submat(boundingRec);
+				int predictNumber = OCRKNN.predict(submat);
+				System.out.println("Predicted: "+ predictNumber);
+				
+				//Imgproc.putText(textMask, "" + predictNumber, new Point(5,5), 0, 1f, new Scalar(0,0,255));
+				ImgShow.imshow("NUMBER", thresholded.submat(boundingRec));
+				
+				//ImgShow.imshow("text", textMask);
+				return predictNumber;
+			}
+		}
+		
+		ImgShow.imshow("predict", thresholded);
+		return 0;
 	}
 	
 	private static Mat subDigit(Mat img, int row, int col) {
---
+++ java/src/club/hcmiuiot/vnOCR/OCRKNN.java
@@ -0,0 +1,161 @@
+package club.hcmiuiot.vnOCR;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.ml.KNearest;
+import org.opencv.ml.Ml;
+import org.opencv.ml.StatModel;
+
+import club.hcmiuiot.opencv.ImgShow;
+
+public class OCRKNN {
+
+	private static KNearest knn = KNearest.create();
+	private static String trainedFile = "trained.txt";
+	
+	public OCRKNN() {
+		//svm.load(trainedFile);
+	}
+	
+	public OCRKNN(String trainedFile) {
+		this.trainedFile = trainedFile;
+		new OCRKNN();
+	}
+	
+	public static void train() {
+		
+		Mat img = new Mat();
+		Mat label = new Mat(0,0, CvType.CV_32SC1);
+
+		Mat imgTrain = new Mat(0,0, CvType.CV_32F);
+		
+		for (int digit=0; digit<10; digit++) {
+			for (int element=1; element <= 35; element++) {
+				img = Imgcodecs.imread("data/digits2/"+digit+"/a (" +  element + ").jpg", 0);
+				System.out.println("data/digits2/"+digit+"/a (" +  element + ").jpg");
+				img.convertTo(img, CvType.CV_32F);
+				img = img.clone().reshape(1,1);
+				//System.out.println(img.dump());
+				label.push_back(new MatOfInt(digit));
+				imgTrain.push_back(img);
+			}			
+		}
+
+		knn.train(imgTrain, Ml.ROW_SAMPLE, label);
+		knn.save(trainedFile);
+		
+		System.out.println(knn.isTrained());	
+	}
+	
+	public static int predict(String fileName) {	
+		Mat a = Imgcodecs.imread(fileName,0);
+		Imgproc.threshold(a, a, 200, 255, Imgproc.THRESH_BINARY);
+		Imgproc.resize(a, a, new Size(20,30));
+		a.convertTo(a, CvType.CV_32F);
+		a = a.clone().reshape(1,1);
+		Mat res = new Mat();
+		knn.findNearest(a, 9, res);
+		//System.out.println("Predicted " + res.dump());
+		return (int) res.get(0, 0)[0];
+	}
+	
+	public static int predict(Mat m) {	
+		//Mat a = Imgcodecs.imread(fileName,0);
+		//Imgproc.threshold(a, a, 200, 255, Imgproc.THRESH_BINARY);
+		Imgproc.resize(m, m, new Size(20,30));
+		m.convertTo(m, CvType.CV_32F);
+		m = m.clone().reshape(1,1);
+		Mat res = new Mat();
+		knn.findNearest(m, 9, res);
+		//System.out.println("Predicted " + res.dump());
+		return (int) res.get(0, 0)[0];
+	}
+	
+	public static void splitAndSave(String fileName) {
+		Mat img = Imgcodecs.imread(fileName);
+		Mat digit = new Mat();
+		
+		Rect rec = new Rect(0, 0, 20, 20);
+		
+		Imgproc.cvtColor(img, img, Imgproc.COLOR_BGR2GRAY);
+		Imgproc.adaptiveThreshold(img, img, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 5, 0);
+		//ImgShow.imshow("digits", img);
+		int c = 0;
+		
+		for (int i=0; i< 50; i++) {
+			for (int j=0; j<100; j++) {
+				rec.x = 20*j;
+				rec.y = 20*i;
+				digit = new Mat(img, rec);
+				Imgcodecs.imwrite("data/"+(i/5)+"/"+c+".jpg", digit, new MatOfInt(Imgcodecs.CV_IMWRITE_PXM_BINARY));
+				//digit.convertTo(digit, );
+				System.out.println("> data/"+(i/5)+"/"+c+".jpg");
+				//train(digit);
+				c++;
+			}
+		}	
+	}
+	
+	public static void test() {
+		Mat img = Imgcodecs.imread("data/digits2.png");
+		Mat gray = new Mat();
+		Mat roi = new Mat();
+		Mat saved = new Mat();
+		Imgproc.cvtColor(img, gray, Imgproc.COLOR_BGR2GRAY);
+		//System.out.println(gray);
+		
+		Imgproc.threshold(gray, gray, 50, 255, Imgproc.THRESH_BINARY);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		Mat hierarchy = new Mat();
+		
+		Imgproc.findContours(gray, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+		for (int i=0; i<contours.size(); i++) {
+			Rect rec = Imgproc.boundingRect(contours.get(i));
+			//Imgproc.rectangle(img, rec.tl(), rec.br(), new Scalar(255,0,0), 2);
+			ImgShow.imshow("a", img.submat(rec));
+			roi = gray.submat(rec);
+			//Imgcodecs.imwrite("data/digits2/"+i+".jpg", gray.submat(rec));
+			
+			float ratio = roi.height()/30.0f;
+			
+//			if (ratio > 1f) {
+//				Imgproc.pyrDown(roi, saved, new Size(roi.width()/ratio, roi.height()/ratio));
+				Imgproc.resize(roi, saved, new Size(20, 30));
+//			}
+//			else
+//				if (ratio < 1f) {
+//					Imgproc.pyrUp(roi, saved, new Size(roi.width()/ratio, roi.height()/ratio));
+//				}
+			
+			Imgcodecs.imwrite("data/digits2/"+i+".jpg", saved);
+			
+			//Imgproc.drawContours(img, contours, i, new Scalar(255,0,0));
+		}
+		
+		Rect rec = Imgproc.boundingRect(contours.get(2));
+		
+		System.out.println(img);
+		
+		ImgShow.imshow("digits2", img);
+	}
+	
+	public int ocr(Mat img) {
+		return 0;
+	}
+	
+}
